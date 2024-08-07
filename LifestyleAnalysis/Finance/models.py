c4c39b8badd988cd923e30666cdada7e2334e172
Troop284 <rybott5000.5@gmail.com>
2024-08-06 23:25:33 -0400

    m

--- LifestyleAnalysis/Finance/models.py
+++ LifestyleAnalysis/Finance/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Transactions(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     fname = models.CharField(max_length=50, null=True, blank=True)  # First Name
     lname = models.CharField(max_length=50, null=True, blank=True)  # Last Name
     phone = models.CharField(max_length=11, null=True, blank=True) 
